0b97ac2141d7ddb74b6320811966ff7fa5e6e19b
Mihai <constantinmihai804@gmail.com>
2026-08-29 12:19:30 +0300

    definire structura

--- app/src/main/java/com/mcvector36/report_loto/ui/main/PlaceholderFragment.kt
+++ app/src/main/java/com/mcvector36/report_loto/ui/main/PlaceholderFragment.kt
@@ -1,57 +1,81 @@
 package com.mcvector36.report_loto.ui.main
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.support.v4.app.Fragment
-import android.arch.lifecycle.Observer
-import android.arch.lifecycle.ViewModelProvider
 import com.mcvector36.report_loto.R
 
-/**
- * A placeholder fragment containing a simple view.
- */
 class PlaceholderFragment : Fragment() {
 
-    private lateinit var pageViewModel: PageViewModel
+    private lateinit var webView: WebView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        pageViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(PageViewModel::class.java).apply {
-            setIndex(arguments?.getInt(ARG_SECTION_NUMBER) ?: 1)
-        }
-    }
-
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
+            inflater: LayoutInflater,
+            container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_main, container, false)
-        val textView: TextView = root.findViewById(R.id.section_label)
-        pageViewModel.text.observe(this, Observer<String> {
-            textView.text = it
-        })
+
+        val root = inflater.inflate(
+                R.layout.fragment_main,
+                container,
+                false
+        )
+
+        webView = root.findViewById(R.id.web_view)
+
+        val webSettings: WebSettings = webView.settings
+
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webSettings.loadWithOverviewMode = true
+        webSettings.useWideViewPort = true
+
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+
+        val sectionNumber =
+                arguments?.getInt(ARG_SECTION_NUMBER) ?: 1
+
+        val url = when (sectionNumber) {
+            1 -> "https://www.loto.ro/?p=3872"
+            2 -> "https://www.loto.ro/?p=3899"
+            else -> "https://www.loto.ro/"
+        }
+
+        webView.loadUrl(url)
+
         return root
     }
 
+    override fun onDestroyView() {
+        webView.stopLoading()
+        webView.webViewClient = null
+        webView.webChromeClient = null
+        webView.destroy()
+
+        super.onDestroyView()
+    }
+
     companion object {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
+
         private const val ARG_SECTION_NUMBER = "section_number"
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
         @JvmStatic
         fun newInstance(sectionNumber: Int): PlaceholderFragment {
             return PlaceholderFragment().apply {
                 arguments = Bundle().apply {
-                    putInt(ARG_SECTION_NUMBER, sectionNumber)
+                    putInt(
+                            ARG_SECTION_NUMBER,
+                            sectionNumber
+                    )
                 }
             }
         }
--- app/src/main/java/com/mcvector36/report_loto/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/mcvector36/report_loto/ui/main/SectionsPagerAdapter.kt
@@ -11,16 +11,12 @@ private val TAB_TITLES = arrayOf(
         R.string.tab_text_2
 )
 
-/**
- * A [FragmentPagerAdapter] that returns a fragment corresponding to
- * one of the sections/tabs/pages.
- */
-class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
-    : FragmentPagerAdapter(fm) {
+class SectionsPagerAdapter(
+        private val context: Context,
+        fm: FragmentManager
+) : FragmentPagerAdapter(fm) {
 
     override fun getItem(position: Int): Fragment {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
         return PlaceholderFragment.newInstance(position + 1)
     }
 
@@ -29,7 +25,6 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
     }
 
     override fun getCount(): Int {
-        // Show 2 total pages.
-        return 2
+        return TAB_TITLES.size
     }
 }
